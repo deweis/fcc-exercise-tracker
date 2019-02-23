fbfa64c228e26e8c51124398b932a1caad7da21e
Dennis <deweis@gmx.ch>
2019-02-23 10:28:22 +0100

    Add GET users endpoint

--- server.js
+++ server.js
@@ -26,12 +26,29 @@ app.get('/api/hello', function(req, res) {
  * Open Stories:
  *
  * 1. I can create a user by posting form data username to /api/exercise/new-user and returned will be an object with username and _id.
- * 2. I can get an array of all users by getting api/exercise/users with the same info as when creating a user.
  * 3. I can add an exercise to any user by posting form data userId(_id), description, duration, and optionally date to /api/exercise/add. If no date supplied it will use current date. Returned will the the user object with also with the exercise fields added.
  * 4. I can retrieve a full exercise log of any user by getting /api/exercise/log with a parameter of userId(_id). Return will be the user object with added array log and count (total exercise count).
  * 5. I can retrieve part of the log of any user by also passing along optional parameters of from & to or limit. (Date format yyyy-mm-dd, limit = int)
  *
  */
+
+// 2. I can get an array of all users by getting api/exercise/users with the same info as when creating a user.
+app.get('/api/exercise/users', function(req, res) {
+  console.log('----------------------- GET REQUEST -----------------------');
+  console.log('Request: GET users');
+  const arrTmp = [
+    {
+      username: 'den',
+      _id: 'aiCsi'
+    },
+    {
+      username: 'den2',
+      _id: 'aiCsi2'
+    }
+  ];
+
+  return res.json(arrTmp);
+});
 
 // Not found middleware
 app.use((req, res, next) => {
