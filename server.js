a520a2ff4705de0c7f4eb092e6ac8f649e5020df
Dennis <deweis@gmx.ch>
2019-02-23 09:54:34 +0100

    Add user stories

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MLAB_URI || 'mongodb://localhost/exercise-track');
+// mongoose.connect(process.env.MLAB_URI || 'mongodb://localhost/exercise-track');
 
 app.use(cors());
 
@@ -42,6 +42,15 @@ app.use((err, req, res, next) => {
     .type('txt')
     .send(errMessage);
 });
+
+/**
+ * 1. I can create a user by posting form data username to /api/exercise/new-user and returned will be an object with username and _id.
+ * 2. I can get an array of all users by getting api/exercise/users with the same info as when creating a user.
+ * 3. I can add an exercise to any user by posting form data userId(_id), description, duration, and optionally date to /api/exercise/add. If no date supplied it will use current date. Returned will the the user object with also with the exercise fields added.
+ * 4. I can retrieve a full exercise log of any user by getting /api/exercise/log with a parameter of userId(_id). Return will be the user object with added array log and count (total exercise count).
+ * 5. I can retrieve part of the log of any user by also passing along optional parameters of from & to or limit. (Date format yyyy-mm-dd, limit = int)
+ *
+ */
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port);
