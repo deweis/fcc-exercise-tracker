88fc1febe846b0ae1df6bfb1a0d8d447ec20807e
Dennis <deweis@gmx.ch>
2019-02-23 10:28:51 +0100

    Add POST new user endpoint

--- server.js
+++ server.js
@@ -25,12 +25,18 @@ app.get('/api/hello', function(req, res) {
 /**
  * Open Stories:
  *
- * 1. I can create a user by posting form data username to /api/exercise/new-user and returned will be an object with username and _id.
  * 3. I can add an exercise to any user by posting form data userId(_id), description, duration, and optionally date to /api/exercise/add. If no date supplied it will use current date. Returned will the the user object with also with the exercise fields added.
  * 4. I can retrieve a full exercise log of any user by getting /api/exercise/log with a parameter of userId(_id). Return will be the user object with added array log and count (total exercise count).
  * 5. I can retrieve part of the log of any user by also passing along optional parameters of from & to or limit. (Date format yyyy-mm-dd, limit = int)
  *
  */
+
+//1. I can create a user by posting form data username to /api/exercise/new-user and returned will be an object with username and _id.
+app.post('/api/exercise/new-user', function(req, res) {
+  console.log('----------------------- POST REQUEST -----------------------');
+  console.log('Request: POST new user: ', req.body);
+  return res.json({ username: 'den', _id: 'aiCsi' });
+});
 
 // 2. I can get an array of all users by getting api/exercise/users with the same info as when creating a user.
 app.get('/api/exercise/users', function(req, res) {
